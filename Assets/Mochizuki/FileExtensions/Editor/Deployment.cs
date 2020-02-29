1ff48faada9c8f7ea762cf3db17270e5e5c2f892
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-01 02:17:54 +0900

    feat: add deployment script

--- Assets/Mochizuki/FileExtensions/Editor/Deployment.cs
+++ Assets/Mochizuki/FileExtensions/Editor/Deployment.cs
@@ -3,13 +3,28 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *------------------------------------------------------------------------------------------*/
 
+using System.IO;
+using System.Linq;
+
+using UnityEditor;
+
 using UnityEngine;
 
+// ReSharper disable once CheckNamespace
 public class Deployment : MonoBehaviour
 {
-    // Use this for initialization
-    private void Start() { }
+    [MenuItem("Mochizuki/FileExtensions/Tools/Export as UnityPackage")]
+    public static void ExportAsUnityPackage()
+    {
+        var path = Path.Combine(Application.dataPath, "Mochizuki");
+        var assets = Directory.GetFiles(path, "*", SearchOption.AllDirectories)
+                              .Where(w => Path.GetExtension(w) == ".cs" && !w.EndsWith("Deployment.cs"))
+                              .Select(w => "Assets" + w.Replace(Application.dataPath, "").Replace("\\", "/"))
+                              .ToArray();
 
-    // Update is called once per frame
-    private void Update() { }
+        if (!Directory.Exists(Path.Combine(Application.dataPath, "Mochizuki/Packages")))
+            Directory.CreateDirectory(Path.Combine(Application.dataPath, "Mochizuki/Packages"));
+
+        AssetDatabase.ExportPackage(assets, "./Assets/Mochizuki/Packages/FileExtensions.unitypackage", ExportPackageOptions.Default);
+    }
 }
